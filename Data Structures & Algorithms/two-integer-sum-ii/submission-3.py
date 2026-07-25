306ea125ebd8bd9b9e743945f36a05bf117a2ffc
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2026-07-25 10:59:41 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # for i in range(len(numbers)):
+        #     for j in range(i+1, len(numbers)):
+        #         if numbers[i] + numbers[j] == target:
+        #             return [i+1, j+1]
+
+        # let us try the binary search solution
+        for i in range(len(numbers)):
+
+            left, right = i, len(numbers)-1
+            temp = target - numbers[i]
+            
+            while left<=right:
+                mid = (left+right)//2
+                if temp == numbers[mid]:
+                    return [i+1, mid+1]
+                elif temp>numbers[mid]:
+                    left+=1
+                else:
+                    right-=1
+        return []
+        
