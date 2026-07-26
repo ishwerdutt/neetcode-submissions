c57c1c040f674dae16cc1b06819b380a0b951f09
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2026-07-26 16:23:04 +0530

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.py
@@ -0,0 +1,58 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        #let us do this two pointer approach
+        # right = len(nums)-1
+        # k = k%(len(nums))
+
+        # while k:
+        #     temp = nums[right]
+        #     j = len(nums) - 1
+            
+        #     while j>0:
+        #         nums[j] = nums[j-1]
+        #         j = j-1
+        #     nums[j] = temp
+        #     k = k-1
+        # print(nums)
+        
+        # time limit is getting exceeded in leetcode
+        # another approach but with some space complexity
+
+
+
+        # temp_arr = [0]*len(nums)
+        # n = len(nums)
+        # k = k%n
+
+        # for i in range(n):
+        #     temp_arr[(i+k)%n] = nums[i]
+        
+
+        # print(temp_arr)
+
+
+        # for i in range(len(nums)):
+        #     nums[i] = temp_arr[i]
+
+        # print(nums)
+        
+
+        # first we will reverse the whole arr
+
+        
+        n = len(nums)
+        k = k%n
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        reverse(0, len(nums)-1)
+        reverse(0, k-1)
+        reverse(k, len(nums)-1)
+
