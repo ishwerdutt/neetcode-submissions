b9a0953d0bce502ba904362a77d9ed16de154fa4
ishwer dutt <96070571+ishwerdutt@users.noreply.github.com>
2026-08-15 18:35:50 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.py
@@ -0,0 +1,12 @@
+class StockSpanner:
+    def __init__(self):
+        # stack stores pairs of (price, accumulated_span)
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            prev_price, prev_span = self.stack.pop()
+            span += prev_span
+        self.stack.append((price, span))
+        return span
